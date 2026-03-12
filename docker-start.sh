6f6cdca61a64c2a5094a65dfebefda8d2293518c
compucol89 <compucol89@gmail.com>
2026-03-12 12:40:03 -0300

    fix: remove all artisan cache commands from startup - Iyzipay class conflict

--- docker-start.sh
+++ docker-start.sh
@@ -6,9 +6,6 @@ cd /app
 # Generar .env desde variables de entorno del contenedor
 printenv | grep -E '^(APP_|DB_|MAIL_|SESSION_|CACHE_|QUEUE_|LOG_|BROADCAST_|VAPID_|FACEBOOK_|GOOGLE_)' > .env
 
-php artisan config:clear
-php artisan route:cache
-php artisan view:cache
 php artisan storage:link --force
 php artisan migrate --force
 
